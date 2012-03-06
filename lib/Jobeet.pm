2f46a2d5d39c884873bab992b0dfe655a0a2bf9d
Mitsuru Onoguchi <gml.onoguchi@gmail.com>
2012-03-06 18:32:32 +0900

    Jobeetのファイルを追加

---
+++ lib/Jobeet.pm
@@ -0,0 +1,34 @@
+package Jobeet;
+use Ark;
+
+use_model 'Jobeet::Models';
+our $VERSION = '0.01';
+
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=head1 NAME
+
+Jobeet -
+
+=head1 SYNOPSIS
+
+use Jobeet;
+
+=head1 DESCRIPTION
+
+Jobeet is
+
+=head1 AUTHOR
+
+Mitsuru Onoguchi E<lt>gml.onoguchi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
